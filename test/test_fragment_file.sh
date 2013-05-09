d9dddc4b857573cd9a00583192e88d945a4dbb39
Matthieu Baerts <matttbe@gmail.com>
2013-05-09 15:52:17 +0200

    Test: added sh file to create frament file

---
+++ test/test_fragment_file.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+DISK=/mnt/disk
+TEST_FILE=$DISK/test.txt
+TMP_FILE_PRE=$DISK/tmp
+TMP_FILE0=${TMP_FILE_PRE}0
+
+if test -d $DISK ; then
+	echo "$DISK already exists, go next"
+else
+	echo "create dir: $DISK"
+	mkdir $DISK
+	/sbin/mkfs.mfs /dev/c0d1
+	mount /dev/c0d1 $DISK
+fi
+
+echo "Create test file: $TEST_FILE"
+dd if=/dev/urandom of=$TEST_FILE count=10 ibs=4096 obs=4096 bs=4096
+
+cp $TEST_FILE $TMP_FILE0
+
+# seq is not available..
+for i in 1 2 3 4 5; do
+	cat $TMP_FILE0 >> $TEST_FILE
+	cp  $TMP_FILE0 $TMP_FILE_PRE$i
+done
+rm $TMP_FILE_PRE*
