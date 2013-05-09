9c258325590e0fb7d9f83924c49e3b42a1f37304
Matthieu Baerts <matttbe@gmail.com>
2013-05-09 10:56:05 +0200

    libc: used _loadname() to do the same thing + check error

--- src/lib/libc/other/_nfrags.c
+++ src/lib/libc/other/_nfrags.c
@@ -1,33 +1,5 @@
-/*
- * Project P3
- * 
- * Copyright 2013 UCLouvain / pole INGI
- *
- * Authors:
- *   Matthieu Baerts <matthieu.baerts@student.uclouvain.be>
- *   Helene Verhaeghe <helene.verhaeghe@student.uclouvain.be>
- * 
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 3 of the License, or
- * (at your option) any later version.
- * 
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- * 
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
- * MA 02110-1301, USA.
- * 
- */
-
 #include <lib.h>
-#define nfrags	_nfrags
 #include <unistd.h>
-#include <string.h> /* strlen */
 
 PUBLIC int nfrags(cFilePath)
 const char *cFilePath;
@@ -35,10 +7,14 @@ const char *cFilePath;
   message m;
   int r;
 
-  m.m1_p1 = (char *)cFilePath;		/* m_in.name1 */
-  m.m1_i1 = strlen(cFilePath) + 1;	/* m_in.name1_length, TODO: can maybe be removed => setgroups */
+  /* This function is used to load a string into a type m3 message. If the
+   * string fits in the message, it is copied there.  If not, a pointer to
+   * it is passed. => loadname.c (e.g. posix/_unlink.c)
+   */
+  _loadname(cFileName, &m);
+
   r = _syscall(VFS_PROC_NR, NFRAGS, &m);
   if (r < 0)
-	return -1; /* we want -1 if there is an error */
+	return -1; /* TODO: we want -1 if there is an error */
   return r;
 }
