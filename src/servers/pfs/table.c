f4319c256d9d62ed8743b73da4207bb6cccf72a7
Matthieu Baerts <matttbe@gmail.com>
2013-05-09 10:57:29 +0200

    PFS: forgot to fill in the table

--- src/servers/pfs/table.c
+++ src/servers/pfs/table.c
@@ -47,6 +47,8 @@ PUBLIC _PROTOTYPE (int (*fs_call_vec[]),
         no_sys,	            /* 30  */
         no_sys,	            /* 31  */
 	no_sys,             /* 32 */
+	no_sys,             /* 33 */
+	no_sys,             /* 34 */
 };
 
 /* Device Handlers (/dev/uds) */
